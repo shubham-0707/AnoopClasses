cab0145f5426ec5e82960240a9396869b69a846c
Shubham Singh <shubhamsinghlmp578@gmail.com>
2021-09-26 18:43:08 +0530

    Added the logout button in proxy activity

--- app/src/main/java/android/example/anoopclassesproject/proxy.java
+++ app/src/main/java/android/example/anoopclassesproject/proxy.java
@@ -2,13 +2,26 @@ package android.example.anoopclassesproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class proxy extends AppCompatActivity {
+
+    ImageView image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_proxy);
+
+        image = findViewById(R.id.logout);
+        image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(proxy.this , MainActivity.class));
+            }
+        });
     }
 }
